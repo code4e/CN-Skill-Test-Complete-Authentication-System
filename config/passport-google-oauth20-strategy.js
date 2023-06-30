0899c20559fe19af2c782ea8a07085e2a8208778
UmarFaiq <umarfaiq@pop-os.localdomain>
2023-06-30 21:08:05 +0530

    change oauth2 policy

--- config/passport-google-oauth20-strategy.js
+++ config/passport-google-oauth20-strategy.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:8000/users/auth/google/callback"
+    callbackURL: `${process.env.BASE_URL}/users/auth/google/callback`
 },
     async function (accessToken, refreshToken, profile, done) {
         try {
